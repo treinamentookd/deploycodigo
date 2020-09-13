20d0c0945b14105a52086731b7bc547d5997c45c
Douglas Ramiro <douglas@cf.coop.br>
2020-09-13 08:55:50 -0300

    add aplicação

---
+++ src/main/java/br/com/tecnisys/deploycodigo/DeploycodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecnisys.deploycodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploycodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploycodigoApplication.class, args);
+	}
+
+}
